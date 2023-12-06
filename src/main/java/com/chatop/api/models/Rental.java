ad3e8152ec39ddb484ce2565f1cf0858aad49b56
Simon <simon.tessard@gmail.com>
2023-12-06 22:41:41 +0100

    Rental model

---
+++ src/main/java/com/chatop/api/models/Rental.java
@@ -0,0 +1,27 @@
+package com.chatop.api.models;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "RENTALS")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Double surface;
+    private Double price;
+    private String picture;
+    private String description;
+    private Integer owner_id;
+    private Timestamp created_at;
+    private Timestamp updated_at;
+}
