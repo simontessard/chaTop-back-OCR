2ae15ff3922fa5f887bfb139b0c0dee24c606562
Simon <simon.tessard@gmail.com>
2023-12-21 17:42:18 +0100

    Formatted TDO

--- src/main/java/com/chatop/api/dto/TokenDTO.java
+++ src/main/java/com/chatop/api/dto/TokenDTO.java
@@ -1,17 +1,14 @@
 package com.chatop.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenDTO {
     private String token;
-
-    public TokenDTO(String token) {
-        this.token = token;
-    }
-
-    public String getToken() {
-        return this.token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
