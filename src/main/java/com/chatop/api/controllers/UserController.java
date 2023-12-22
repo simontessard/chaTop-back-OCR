0528a9b6bb504014ac6bc9746c0fbbaba3951cc0
Simon <simon.tessard@gmail.com>
2023-12-22 14:14:59 +0100

    Added get user by id route

--- src/main/java/com/chatop/api/controllers/UserController.java
+++ src/main/java/com/chatop/api/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.chatop.api.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.chatop.api.dto.UserDetailsDTO;
 import com.chatop.api.models.User;
@@ -19,12 +22,27 @@ public class UserController {
     private final UserService userService;
 
     @Operation(summary = "Get the authenticated user's details")
-
     @GetMapping("api/auth/me")
     public ResponseEntity<UserDetailsDTO> getAuthenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
         User user = userService.getUserByUsername(username);
+        UserDetailsDTO userDetails = new UserDetailsDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getUpdatedAt());
+        return ResponseEntity.ok(userDetails);
+    }
+
+    @Operation(summary = "Get user's details by id")
+    @GetMapping("api/user/{id}")
+    public ResponseEntity<UserDetailsDTO> getUserById(@PathVariable Integer id) {
+        User user = userService.getUserById(id);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
         UserDetailsDTO userDetails = new UserDetailsDTO(
                 user.getId(),
                 user.getName(),
