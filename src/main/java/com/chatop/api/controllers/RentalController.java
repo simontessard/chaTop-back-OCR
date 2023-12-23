ba13378a1dd12c29bc340e93e4d95e099ece086b
Simon <simon.tessard@gmail.com>
2023-12-23 17:58:50 +0100

    Removed useless import

--- src/main/java/com/chatop/api/controllers/RentalController.java
+++ src/main/java/com/chatop/api/controllers/RentalController.java
@@ -16,10 +16,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.chatop.api.dto.RentalCreateDTO;
 import com.chatop.api.models.Rental;
