407326d0a1fc299783a5b9b3dd13b6ea8ae2d50a
Simon <simon.tessard@gmail.com>
2023-12-27 19:17:29 +0100

    Moved data management to service

--- src/main/java/com/chatop/api/controllers/MessageController.java
+++ src/main/java/com/chatop/api/controllers/MessageController.java
@@ -6,13 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chatop.api.models.Message;
-import com.chatop.api.models.Rental;
-import com.chatop.api.models.User;
-
 import com.chatop.api.services.MessageService;
-import com.chatop.api.services.RentalService;
-import com.chatop.api.services.UserService;
-
 import io.swagger.v3.oas.annotations.Operation;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,30 +19,11 @@ import com.chatop.api.dto.MessageDTO;
 @RequiredArgsConstructor
 public class MessageController {
     private final MessageService messageService;
-    private final UserService userService;
-    private final RentalService rentalService;
 
     @Operation(summary = "Create a message between an user and a rental")
     @PostMapping("/api/messages")
     public ResponseEntity<Message> createMessage(@Valid @RequestBody MessageDTO payload) {
-        String message = payload.getMessage();
-        Integer userId = payload.getUserId();
-        Integer rentalId = payload.getRentalId();
-
-        if (userId == null || rentalId == null) {
-            throw new IllegalArgumentException("User ID and Rental ID must not be null");
-        }
-
-        User user = userService.getUserById(userId);
-        Rental rental = rentalService.getRentalById(rentalId)
-                .orElseThrow(() -> new RuntimeException("Rental not found"));
-
-        Message newMessage = new Message();
-        newMessage.setMessage(message);
-        newMessage.setUser(user);
-        newMessage.setRental(rental);
-
-        Message savedMessage = messageService.createMessage(newMessage);
+        Message savedMessage = messageService.createMessage(payload);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedMessage);
     }
 }
--- src/main/java/com/chatop/api/services/MessageService.java
+++ src/main/java/com/chatop/api/services/MessageService.java
@@ -4,7 +4,10 @@ import java.sql.Timestamp;
 
 import org.springframework.stereotype.Service;
 
+import com.chatop.api.dto.MessageDTO;
 import com.chatop.api.models.Message;
+import com.chatop.api.models.Rental;
+import com.chatop.api.models.User;
 import com.chatop.api.repository.MessageRepository;
 
 /**
@@ -16,6 +19,8 @@ public class MessageService {
      * The repository used for interacting with messages in the database.
      */
     private final MessageRepository messageRepository;
+    private final UserService userService;
+    private final RentalService rentalService;
 
     /**
      * Constructor for the MessageService class.
@@ -23,8 +28,10 @@ public class MessageService {
      * @param messageRepository The repository to use for interacting with messages
      *                          in the database.
      */
-    public MessageService(MessageRepository messageRepository) {
+    public MessageService(MessageRepository messageRepository, UserService userService, RentalService rentalService) {
         this.messageRepository = messageRepository;
+        this.userService = userService;
+        this.rentalService = rentalService;
     }
 
     /**
@@ -36,9 +43,26 @@ public class MessageService {
      * @param newMessage The message to create.
      * @return The created message.
      */
-    public Message createMessage(Message newMessage) {
+    public Message createMessage(MessageDTO payload) {
+        String message = payload.getMessage();
+        Integer userId = payload.getUserId();
+        Integer rentalId = payload.getRentalId();
+
+        if (userId == null || rentalId == null) {
+            throw new IllegalArgumentException("User ID and Rental ID must not be null");
+        }
+
+        User user = userService.getUserById(userId);
+        Rental rental = rentalService.getRentalById(rentalId)
+                .orElseThrow(() -> new RuntimeException("Rental not found"));
+
+        Message newMessage = new Message();
+        newMessage.setMessage(message);
+        newMessage.setUser(user);
+        newMessage.setRental(rental);
         newMessage.setCreated_at(new Timestamp(System.currentTimeMillis()));
         newMessage.setUpdated_at(newMessage.getCreated_at());
+
         return messageRepository.save(newMessage);
     }
 
